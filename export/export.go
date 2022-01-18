4d9a6a9e3b39d12a35201beb6419ed067e314c8f
dongpeng <rowzixin@163.com>
2022-01-18 17:05:24 +0800

    export file

--- export/export.go
+++ export/export.go
@@ -29,23 +29,52 @@ var ExportsCmd = &cli.Command{
 			Usage:    "Filecoin Miner. Such as: f01000",
 			Required: true,
 		},
+		&cli.StringFlag{
+			Name:     "sector-numbers",
+			Usage:    "specify the sector numbers file for the sectors recovery",
+		},
+		&cli.Int64Flag{
+			Name: "sector-count",
+			Usage: "specify sector count per meta file",
+		},
 	},
 	Action: func(cctx *cli.Context) error {
 		ctx := cliutil.ReqContext(cctx)
 		start := time.Now()
 
-		if cctx.Args().Len() < 1 {
-			return fmt.Errorf("at least one sector must be specified")
+		if cctx.Args().Len() < 1 && !cctx.IsSet("sector-numbers") {
+			return fmt.Errorf("at least one sector must be specified or sector numbers json file")
 		}
 
 		runSectors := make([]uint64, 0)
-		for _, sn := range cctx.Args().Slice() {
-			sectorNum, err := strconv.ParseUint(sn, 10, 64)
+
+		if cctx.IsSet("sector-numbers") {
+			of, err := homedir.Expand(cctx.String("sector-numbers"))
 			if err != nil {
-				return fmt.Errorf("could not parse sector number: %w", err)
+				return xerrors.Errorf("get homedir: %v", err)
 			}
-			runSectors = append(runSectors, sectorNum)
+
+			b, err := ioutil.ReadFile(of)
+			if err != nil {
+				return xerrors.Errorf("ioutil ReadFile: %v", err)
+			}
+
+			if err := json.Unmarshal(b, &runSectors); err != nil {
+				return xerrors.Errorf("json Unmarshal: %v", err)
+			}
 		}
+
+		if cctx.Args().Len() >= 1 {
+			for _, sn := range cctx.Args().Slice() {
+				sectorNum, err := strconv.ParseUint(sn, 10, 64)
+				if err != nil {
+					return fmt.Errorf("could not parse sector number: %w", err)
+				}
+				runSectors = append(runSectors, sectorNum)
+			}
+		}
+
+		fmt.Printf("sector counts: %d\n", len(runSectors))
 
 		maddr, err := address.NewFromString(cctx.String("miner"))
 		if err != nil {
@@ -64,40 +93,78 @@ var ExportsCmd = &cli.Command{
 			return xerrors.Errorf("Getting StateMinerInfo err:", err)
 		}
 
+		sectorCount := cctx.Int("sector-count")
+
 		output := &RecoveryParams{
 			Miner:      maddr,
 			SectorSize: mi.SectorSize,
 		}
 		sectorInfos := make(SectorInfos, 0)
-		failtSectors := make([]uint64, 0)
+		failedSectors := make([]uint64, 0)
+
+		idx := 1
+		count := 0
 		for _, sector := range runSectors {
 			ts, sectorPreCommitOnChainInfo, err := GetSectorCommitInfoOnChain(ctx, fullNodeApi, maddr, abi.SectorNumber(sector))
 			if err != nil {
 				log.Errorf("Getting sector (%d) precommit info error: %v ", sector, err)
+				failedSectors = append(failedSectors, sector)
 				continue
 			}
+			ticket, err := GetSectorTicketOnChain(ctx, fullNodeApi, maddr, ts, sectorPreCommitOnChainInfo)
+			if err != nil {
+				log.Errorf("Getting sector (%d) ticket error: %v ", sector, err)
+				failedSectors = append(failedSectors, sector)
+				continue
+			}
+
 			si := &SectorInfo{
 				SectorNumber: abi.SectorNumber(sector),
 				SealProof:    sectorPreCommitOnChainInfo.Info.SealProof,
 				SealedCID:    sectorPreCommitOnChainInfo.Info.SealedCID,
+				Ticket:       ticket,
 			}
-
-			ticket, err := GetSectorTicketOnChain(ctx, fullNodeApi, maddr, ts, sectorPreCommitOnChainInfo)
-			if err != nil {
-				log.Errorf("Getting sector (%d) ticket error: %v ", sector, err)
-				continue
-			}
-			si.Ticket = ticket
-
+			// to test
+			//si := &SectorInfo{
+			//	SectorNumber: abi.SectorNumber(sector),
+			//}
 			sectorInfos = append(sectorInfos, si)
-			output.SectorInfos = sectorInfos
 
+			count++
+			if sectorCount != 0 && count >= sectorCount {
+				output.SectorInfos = sectorInfos
+				out, err := json.MarshalIndent(output, "", "\t")
+				if err != nil {
+					return err
+				}
+
+				filePath := fmt.Sprintf("sectors-recovery-%s-%d.json", maddr.String(), idx)
+				of, err := homedir.Expand(filePath)
+				if err != nil {
+					return err
+				}
+
+				if err := ioutil.WriteFile(of, out, 0644); err != nil {
+					return err
+				}
+
+				fmt.Printf("file idx: %d, export: %d\n", idx, len(sectorInfos))
+
+				idx++
+				count = 0
+				sectorInfos = make(SectorInfos, 0)
+			}
+		}
+
+		if len(sectorInfos) > 0 {
+			output.SectorInfos = sectorInfos
 			out, err := json.MarshalIndent(output, "", "\t")
 			if err != nil {
 				return err
 			}
 
-			of, err := homedir.Expand("sectors-recovery-" + maddr.String() + ".json")
+			filePath := fmt.Sprintf("sectors-recovery-%s-%d.json", maddr.String(), idx)
+			of, err := homedir.Expand(filePath)
 			if err != nil {
 				return err
 			}
@@ -105,10 +172,12 @@ var ExportsCmd = &cli.Command{
 			if err := ioutil.WriteFile(of, out, 0644); err != nil {
 				return err
 			}
+
+			fmt.Printf("file idx: %d, export: %d\n", idx, len(sectorInfos))
 		}
 
 		end := time.Now()
-		fmt.Println("export", len(sectorInfos), "sectors, failt sectors:", failtSectors, ", elapsed:", end.Sub(start))
+		fmt.Println("failed sectors:", failedSectors, ", elapsed:", end.Sub(start))
 
 		return nil
 	},
